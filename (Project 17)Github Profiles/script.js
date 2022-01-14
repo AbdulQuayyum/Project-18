242b92d8b66e2c630939e151118e24b2d65178cf
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2022-01-14 05:10:43 +0100

    first commit

--- (Project 17)Github Profiles/script.js
+++ (Project 17)Github Profiles/script.js
@@ -6,7 +6,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-getUser("florinpop17");
+getUser("AbdulQuayyum");
 
 async function getUser(username) {
   const resp = await fetch(APIURL + username);
